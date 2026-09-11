0c67ac1d442e9773e30b477cd27d1e0797caa755
dastrikerknight <shivamscientist9811@gmail.com>
2026-09-11 11:58:03 +0530

    obj

--- day6/o.js
+++ day6/o.js
@@ -11,3 +11,22 @@ function updatemarks(n){
 }
 a.updatemark(70);
 console.log(a.marks);
+function add(...numb){
+    console.log(numb);
+}
+add(12,32,54,23,5,3,5,2,9);
+function namto(name,...num){
+    let sum=num.reduce((sum,num)=>sum+num,0);
+    console.log("Hello "+name);
+    console.log(sum);
+}
+namto('ko',23,45,89,23,7,1,90);
+
+const sumAll = (...numbers) => {
+  return numbers.reduce((total, current) => total + current, 0);
+};
+const userInput = "23,45,78,9,1,67,54";
+const stringArray = userInput.split(",");
+const numberArray = stringArray.map(num => Number(num.trim()));
+const result = sumAll(...numberArray);
+console.log(result);
